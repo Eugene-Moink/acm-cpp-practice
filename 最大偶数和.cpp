8c44c3231d36905a483c7a222477606ff8251543
moink <28929253@qq.com>
2026-06-14 22:58:04 +0800

    add new cpp

--- 最大偶数和.cpp
+++ 最大偶数和.cpp
@@ -5,71 +5,33 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     ll t;
     cin >> t;
     while(t--)
     {
-        ll a, b;
-        cin >> a >> b;
-        if ((a + b) % 2 == 0)  
-        {
-            ll op1 = a + b;  
-            ll max_val = op1;  
-            
-            
-            for(ll k = 1; k * k <= b; k++)
-            {
-                if (b % k == 0)
-                {
-                    ll new_a = a * k;
-                    ll new_b = b / k;
-                    ll new_sum = new_a + new_b;
-                    
-                    if (new_sum % 2 == 0)
-                    {
-                        max_val = max(max_val, new_sum);
-                    }
-                    
-                    ll k2 = b / k;
-                    if (k2 != k) 
-                    {
-                        new_a = a * k2;
-                        new_b = b / k2;
-                        new_sum = new_a + new_b;
-                        
-                        if (new_sum % 2 == 0) 
-                        {
-                            max_val = max(max_val, new_sum);
-                        }
-                    }
-                }
-            }
-            cout << max_val << endl;
+        ll b, a;  // 注意：这里是b,a顺序，和Python一样
+        cin >> b >> a;
+        
+        ll sol = -1;
+        
+        // 条件1：如果a+b是偶数
+        if ((a + b) % 2 == 0) {
+            sol = a + b;
         }
-        else
-        {
-            if (a % 2 == 0 &&b % 2 == 1)
-            {
-                cout << "-1" << endl; 
-            }
-            else if(a % 2 == 1 &&b % 2 == 0)
-            {
-                if (b % 4 == 0)
-                {
-                    ll a1 = a * 2;
-                    ll b1 = b / 2;
-                    cout << a1 + b1 << endl;
-                }
-                else
-                {
-                    cout << "-1" << endl;
-                }
-            }
-            else
-            {
-                cout << "-1" << endl;
-            }
+        
+        // 条件2：如果a和b都是奇数
+        if (a % 2 == 1 && b % 2 == 1) {
+            sol = max(sol, a * b + 1);
         }
+        // 条件3：如果a是偶数且(a能被4整除或b是偶数)
+        else if (a % 2 == 0 && (a % 4 == 0 || b % 2 == 0)) {
+            sol = max(sol, 2 + (a * b) / 2);
+        }
+        
+        cout << sol << endl;
     }
     return 0;
 }
